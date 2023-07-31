f8864aede42ddb8679fae9e554580e6f1f409e64
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-07-31 14:12:55 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  let str = number.toString();
+  let result = '';
+  const hundreds = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+  const dozens = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const integer = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+  if (str.length == 1) {
+    return units[number];
+  }
+  else if (str.length == 2) {
+    if (str[0] == 1) {
+      result = integer[parseInt(str[1])]
+    } else {
+      result = dozens[parseInt(str[0]) - 1] + ((str[1] != '0') ? (' ' + units[parseInt(str[1])]) : '');
+    }
+  }
+  else if (str.length == 3) {
+    if (str[1] == 1) {
+      result = hundreds[parseInt(str[0]) - 1] + ((str[1] != '0') ? (' ' + (integer[parseInt(str[2])])) : '');
+    } else {
+      result = hundreds[parseInt(str[0]) - 1] + ((str[1] != '0') ? (' ' + (dozens[parseInt(str[1]) - 1])) : '') + ((str[2] != '0') ? (' ' + units[parseInt(str[2])]) : '');
+    }
+  }
+
+  return result.split('').join('')
 }
+
